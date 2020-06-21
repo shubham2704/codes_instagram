e50f322cadd75baabd9bd881effdd8feee5520d6
shubham2704 <cool.shubham2704@gmail.com>
2020-06-21 19:21:14 +0530

    Added Description to bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,3 +1,9 @@
+"""
+Description 
+It is a comparison-based algorithm in which each pair of adjacent elements is compared and the elements are swapped if 
+they are not in order.
+"""
+
 def bubblesort(list):
     for iter_num in range(len(list)-1,0,-1):
         for idx in range(iter_num):
