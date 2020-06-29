ab64f58c30e343a895f1defd2a54c82b446b5d9c
shubham2704 <cool.shubham2704@gmail.com>
2020-06-29 20:51:52 +0530

    29/06/2020 added celcius to fehernite

---
+++ celciusToFehernit.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print('%.2f Celsius is: %0.2f Fahrenheit' %(celsius, fahrenheit))
+
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print('%.2f Fahrenheit is: %0.2f Celsius' %(fahrenheit, celsius))
