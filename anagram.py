72ca46acd7cbec72209fa2f6e5a53d13d7dbc18e
shubham2704 <cool.shubham2704@gmail.com>
2020-07-25 23:40:02 +0530

    25/07/2020 anagram strings

---
+++ anagram.py
@@ -0,0 +1,13 @@
+"""
+Description 
+An anagram of a string is another string that contains same characters, only the order of characters can be different. For example, “abcd” and “dabc” are anagram of each other.
+
+"""
+
+s1 = input('Enter first string')
+s2 = input('Enter seconf string')
+
+if sorted(s1)==sorted(s2):
+    print('Strings are anagrams')
+else:
+    print('Strings are not annagrams')
